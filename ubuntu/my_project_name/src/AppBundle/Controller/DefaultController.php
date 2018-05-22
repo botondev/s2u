0a7a87bc82c609836571b3a868b9734c16e98b51
Botond Bertalan <botond.bertalan@innonic.com>
2018-05-23 00:45:38 +0200

    This url was followed along: http://symfony.com/doc/2.8/doctrine/associations.html

--- ubuntu/my_project_name/src/AppBundle/Controller/DefaultController.php
+++ ubuntu/my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,12 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +22,69 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
+    }
+
+    /**
+     * @Route("/create_product/", name="createProduct")
+     */
+    public function createProductAction()
+    {
+        $category = new Category();
+        $category->setName('Computer Peripherials');
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(19.99);
+        $product->setDescription('Ergonomic and stylish');
+
+        // relates this product to the category
+        $product->setCategory($category);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($category);
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new Response(
+            'Saved new product with id: '.$product->getId()
+            .' and new category with id: '.$category->getId()
+        );
+    }
+
+    /**
+     * @Route("/show_product/{productId}/", name="showProduct")
+     */
+    public function showAction($productId)
+    {
+        // lazy loaded
+//        $product = $this->getDoctrine()
+//            ->getRepository('\AppBundle\Entity\Product')
+//            ->find($productId);
+
+        $product = $this->getDoctrine()
+            ->getRepository('\AppBundle\Entity\Product')
+            ->findOneByIdJoinedToCategory($productId);
+
+        $categoryName = $product->getCategory()->getName();
+
+        return new Response('category name is '.$categoryName);
+    }
+
+    /**
+     * @Route("/show_products/{categoryId}/", name="showProducts")
+     */
+    public function showProductsAction($categoryId)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository('\AppBundle\Entity\Category')
+            ->find($categoryId);
+
+        $products = $category->getProducts();
+
+        $dummy = new Category();
+        $dummy->setName('dummy yeah ?');
+
+        // TODO: it does not converts objects to json, only primitive types
+        return new JsonResponse($dummy);
     }
 }
---
+++ ubuntu/my_project_name/src/AppBundle/Entity/Product.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var decimal
+     *
+     * @ORM\Column(name="price", type="decimal")
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string")
+     */
+    private $description;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setPrice($price)
+    {
+        return $this->price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
+
