8c2b5988ff40f6ad0fc566607d9a0791dcc96d92
botondev <bbotond90@yahoo.com>
2018-06-05 23:21:05 +0200

    [ubuntu/symfony2blog] Session 9, video 50 is finished with minor changes because of incompatible code

--- ubuntu/symfony2blog/src/Blog/CoreBundle/Controller/PostController.php
+++ ubuntu/symfony2blog/src/Blog/CoreBundle/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace Blog\CoreBundle\Controller;
 
+use Blog\ModelBundle\Entity\Comment;
 use Blog\ModelBundle\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -76,13 +77,46 @@ class PostController extends Controller
      * @param Request $request
      * @param string $slug
      *
+     * @throws NotFoundHttpException
+     * @return array
+     *
      * @Route("/{slug}/create-comment")
      * @Method("POST")
      * @Template("CoreBundle:Post:show.html.twig")
      */
-    public function createCommentAction(Request $request, $slug)
+    public function createCommentAction($slug)
     {
-        return array();
+        $post = $this->getDoctrine()->getRepository('ModelBundle:Post')->findOneBy(
+            array(
+                'slug' => $slug
+            )
+        );
+
+        if ($post === null) {
+            throw $this->createNotFoundException('Post was not found');
+        }
+
+        $comment = new Comment();
+        $comment->setPost($post);
+
+        $form = $this->createForm(new CommentType(), $comment);
+        $form->handleRequest($this->getRequest());
+
+        if ($form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($comment);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Your comment was submitted successfully.');
+
+            return $this->redirect($this->generateUrl('blog_core_post_show', array(
+                'slug' => $post->getSlug()
+            )));
+        }
+
+        return array(
+            'post' => $post,
+            'form' => $form->createView()
+        );
     }
 
 }
