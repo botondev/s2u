6c7e1ceebd69be46d514bb1cb34d82fda2af9b1f
botondev <bbotond90@yahoo.com>
2018-06-19 23:33:14 +0200

    [ubuntu/symfony2blog] fos_userbundle installed and fos_user migrated

--- ubuntu/symfony2blog/app/autoload.php
+++ ubuntu/symfony2blog/app/autoload.php
@@ -1,11 +1,13 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+AnnotationReader::addGlobalIgnoredName('author');
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
---
+++ ubuntu/symfony2blog/src/Blog/ModelBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: botondev
+ * Date: 2018.06.19.
+ * Time: 22:06
+ */
+
+namespace Blog\ModelBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
